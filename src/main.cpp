737dde63bde706a6233e0caaf0a71b254da9f7bd
IMaxiM03I <maxilea202@gmail.com>
2024-11-17 21:15:09 +0100

    add all FCS

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,41 @@
 #include <Arduino.h>
-
 #include <FlightControlSurface.h>
 
-Servo servo_test;
-FlightControlSurface fcs_test = FlightControlSurface(&servo_test);
+// servos
+Servo FLAP_LEFT_SERVO;
+Servo FLAP_RIGHT_SERVO;
+
+Servo RUDDER_LEFT_SERVO;
+Servo RUDDER_RIGHT_SERVO;
+
+Servo ELEVATOR_LEFT_SERVO;
+Servo ELEVATOR_RIGHT_SERVO;
+
+// flight control surfaces
+FlightControlSurface flap_left = FlightControlSurface(&FLAP_LEFT_SERVO);
+FlightControlSurface flap_right = FlightControlSurface(&FLAP_RIGHT_SERVO);
+
+FlightControlSurface rudder_left = FlightControlSurface(&RUDDER_LEFT_SERVO);
+FlightControlSurface rudder_right = FlightControlSurface(&RUDDER_RIGHT_SERVO);
+
+FlightControlSurface elevator_left = FlightControlSurface(&ELEVATOR_LEFT_SERVO);
+FlightControlSurface elevator_right = FlightControlSurface(&ELEVATOR_RIGHT_SERVO);
 
 void setup() {
-  fcs_test.init(9);
+  // attach servos
+  flap_left.init(3);
+  flap_right.init(5);
+
+  rudder_left.init(6);
+  rudder_right.init(9);
+
+  elevator_left.init(10);
+  elevator_right.init(11);
 }
 
 void loop() {
-  fcs_test.center();
+  rudder_right.center();
   delay(2000);
-  fcs_test.move(180);
+  rudder_right.move(180);
   delay(2000);
 }
